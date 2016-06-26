7a3c11c6e44958e5f522dfdadb461cf614f29635
David Young <deedubwhy@gmail.com>
2016-06-26 14:56:51 +1000

    add some comments

--- pub/scripts/pageload.php
+++ pub/scripts/pageload.php
@@ -1,11 +1,12 @@
 <?php
 $link = $_SERVER['REQUEST_URI'];
-
+//were we linked to a shortened url?
 if ($link != '/' ) {
 	include('pub/scripts/db_conf.php');
 	include('pub/scripts/db_conn.php');
 	$db = new MySQLDatabase();
 	$db->connect(DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+	//lookup the url we got sent
 	$stmt = $db->link->prepare("SELECT link_url FROM links WHERE (link_id = ?)");
 	$stmt->bind_param("i", $linkStuff);
 	$linkStuff = (int)to10(explode('/',$link)[1]);
@@ -15,9 +16,11 @@ if ($link != '/' ) {
 	mysqli_stmt_fetch($stmt);
 	$stmt->close();
 	$db->disconnect();
+	//perform the redirect
 	header("Location: ".$url);
 }
 
+//convert ID int in base62 to base10
 function to10($num, $b=62) {
 	$base='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
 	$limit = strlen($num);
--- pub/scripts/shorty.php
+++ pub/scripts/shorty.php
@@ -1,27 +1,34 @@
 <?php
+//get the post variable sent to us
 $longLink = $_POST['link'];
+//make sure it contains http, otherwise add it
 if (!strrpos($longLink, 'http')) {
 	$longLink = 'http://'.$longLink;
 }
+//are we linking to ourselves?
 $pos = strrpos($longLink, "dwy.io");
 if ($pos === false) {
 	include('db_conf.php');
 	include('db_conn.php');
 	$db = new MySQLDatabase();
 	$db->connect(DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+	//insert it into our database
 	$stmt = $db->link->prepare("INSERT INTO links (`link_url`) VALUES (?)");
 	$stmt->bind_param("s", $longLink);
 	$stmt->execute();
 	$newID = $stmt->insert_id;
 	$parsedID = toBase($newID);
+	//return json data to browser with link
 	$arr = array('link' => $parsedID, 'status' => 1);
 	echo json_encode($arr);
 	$stmt->close();
 	$db->disconnect();
 } else {
+	//don't link to ourselves
 	echo json_encode(array('status' => 0));
 }
 
+//convert ID int in base10 to base62
 function toBase($num, $b=62) {
 	$base='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
 	$r = $num  % $b ;
