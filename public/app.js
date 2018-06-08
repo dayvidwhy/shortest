e1a0959dc794d2741c765b909456891253185b93
dayvidwhy <dayvidwhy@gmail.com>
2018-06-08 21:11:53 +1000

    Fix style issues.

--- public/app.js
+++ public/app.js
@@ -1,35 +1,16 @@
-// go get a short url
-function formSubmitted (event) {
-    event.preventDefault();
-    var link = $('input[name=searching]').val();
-
-    // do some validation
-    if (link.indexOf('shortest.glitch.me') >= 0) {
-        $("input").val("Try entering a link to somewhere else.");
-        return;
-    }
-
-    // can't be empty
-    if (link === "" || link === null) {
-        $("input").val("Enter a url first.");
-        return;
-    }
-
-    // does it start with http://
-    if ((link.indexOf('http') !== 0)) {
-        link = 'http://' + link;
-    }
-
-    // make the request
+// send the request
+function makeRequest (link) {
     $.ajax({
-        url: '/encode',
-        method: 'POST',
-        data : {'link' : link},
-        dataType: 'json',
-        beforeSend: function() {
+        url: "/encode",
+        method: "POST",
+        data : {
+            link
+        },
+        dataType: "json",
+        beforeSend: () => {
             $("input").val("working on it");
         },
-        success: function(data) {
+        success: (data) => {
             if (data.status === 1) {
                 $("#input-box").val("https://shortest.glitch.me/"+data.link);
                 $("#submit-btn").val("Done!");
@@ -38,10 +19,36 @@ function formSubmitted (event) {
                 $("#submit-btn").val(":(!");
             }
         },
-        error: function(err) {
+        error: (err) => {
             $("input").val("didn't work :(");
         }
     });
+}
+
+// go get a short url
+function formSubmitted (event) {
+    event.preventDefault();
+    let link = $("input[name=searching]").val();
+
+    // do some validation
+    if (link.indexOf("shortest.glitch.me") >= 0) {
+        $("input").val("Try entering a link to somewhere else.");
+        return;
+    }
+
+    // can"t be empty
+    if (link === "" || link === null) {
+        $("input").val("Enter a url first.");
+        return;
+    }
+
+    // does it start with http://
+    if ((link.indexOf("http") !== 0)) {
+        link = "http://" + link;
+    }
+
+    // make the request
+    makeRequest(link);
 }
 
 // bind listener
--- server.js
+++ server.js
@@ -1,36 +1,35 @@
 // init project
-var express = require('express');
-var bodyParser = require('body-parser');
+var express = require("express");
+var bodyParser = require("body-parser");
 var app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // setup static directory
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // start database
-var fs = require('fs');
-var dbFile = './.data/sqlite.db';
-var exists = fs.existsSync(dbFile);
-var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database(dbFile);
+var fs = require("fs");
+var exists = fs.existsSync("./.data/sqlite.db");
+var sqlite3 = require("sqlite3").verbose();
+var db = new sqlite3.Database("./.data/sqlite.db");
 
 // setup database
 db.serialize(function () {
     if (!exists) {
-        db.run('CREATE TABLE IF NOT EXISTS links (url TEXT)');
+        db.run("CREATE TABLE IF NOT EXISTS links (url TEXT)");
     }
 });
 
 // default view
 app.get("/", function (request, response) {
-    response.sendFile(__dirname + '/views/index.html');
+    response.sendFile(__dirname + "/views/index.html");
 });
 
 // requesting an encoded url
 app.post("/encode", function (request, response) {
     // convert number back to base 62
     function toBase62(num) {
-        const base = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        const base = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
         const baseIndex = num % 62 ;
 
         // pick first number
@@ -47,7 +46,7 @@ app.post("/encode", function (request, response) {
         // final result
         return result;
     }
-    console.log(request.body.link);
+
     // put link in database
     db.run(
         "INSERT INTO links VALUES ($link)",
@@ -56,7 +55,6 @@ app.post("/encode", function (request, response) {
         },
         function (err, info) {
             var that = this;
-            console.log(that.lastID);
             // return encoded
             response.send(JSON.stringify({
                 status: 1,
@@ -70,8 +68,8 @@ app.post("/encode", function (request, response) {
 app.get("/:encoded", function (request, response) {
     // Convert the shortened URL from base 62 back to base 10
     function toBase10(num) {
-        const base = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-        num += '';
+        const base = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        num += "";
         const limit = num.length;
         let result = base.indexOf(num[0]);
         for (let i = 1; i < limit; i++) {
@@ -80,17 +78,16 @@ app.get("/:encoded", function (request, response) {
         return result;
     }
 
+    // get from database
     var rowID = toBase10(request.params.encoded);
-    console.log(rowID);
     db.get(
         "SELECT url FROM links WHERE (rowID = $rowID)",
         {
             $rowID: rowID
         },
         function (err, row) {
-            if (row !== undefined) {
+            if (row) {
                 var that = this;
-                console.log(row);
                 response.redirect(row.url);
             } else {
                 response.redirect("/");
@@ -101,5 +98,5 @@ app.get("/:encoded", function (request, response) {
 
 // listen for requests
 var listener = app.listen(process.env.PORT, function () {
-    console.log('Your app is listening on port ' + listener.address().port);
+    console.log("Your app is listening on port " + listener.address().port);
 });
