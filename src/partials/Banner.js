126bca05b95a4275f56a43d1dfd0d2e592afb887
dayvidwhy <dayvidwhy@gmail.com>
2021-07-31 18:13:41 +1000

    Added emotion css in js, emotion/styled, simple styled component.

--- src/partials/Banner.js
+++ src/partials/Banner.js
@@ -1,10 +1,15 @@
 import React from "react";
 import Type from "@/components/Type.js";
 import Link from "@/components/Link.js";
+import styled from "@emotion/styled";
+
+const Header = styled.header`
+    background-color: #4f79a2;
+`;
 
 const Banner = () => {
     return (
-        <header className="grid-fluid blue-bg">
+        <Header className="grid-fluid">
             <div className="row center space">
                 <Link destination="http://davidyoung.tech">
                     <Type className="banner-text banner-link" element="span">
@@ -12,7 +17,7 @@ const Banner = () => {
                     </Type>
                 </Link>
             </div>
-        </header>
+        </Header>
     );
 };
 
