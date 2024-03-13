2279768a21c2f1172ba6b64f8799aa1df8a35f21
dayvidwhy <dayvidwhy@gmail.com>
2024-03-13 21:12:54 +1000

    Fix comment.

--- packages/validate/validate.ts
+++ packages/validate/validate.ts
@@ -1,13 +1,5 @@
-/*
-    Turns a raw string that might be a url
-    into an actual url if possible.
-
-    This is duplicated from the node servers validate
-    code, because I'm unsure if I want to import something from
-    the front ends source into the servers code.
-
-    A monorepo with shared packages would solve this.
-*/
+// Turns a raw string that might be a url
+// into an actual url if possible.
 export const validatedAddress = (rawUrl: string): boolean => {
     try {
         new URL(rawUrl);
